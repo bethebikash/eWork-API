32c6db5a11c00df834ec607da9b7f4c1b2bf4286
bethebikash <bhattaraibikash55@gmail.com>
2020-06-16 23:19:29 +0545

    User Login completed

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema(
   {
@@ -68,6 +69,15 @@ const userSchema = new mongoose.Schema(
     timestamps: true
   }
 )
+
+// Hashing the plain text password before saving.
+userSchema.pre('save', async function(next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
 
 const User = mongoose.model('User', userSchema)
 
--- src/routes/User.js
+++ src/routes/User.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = new express.Router()
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
-const UserController = require('../controllers/User')
+const UserController = require('../controllers/UserController')
 
-// to create a user
+// to create a user // User Registation
 router.post('/users', UserController.checkIfUserExist, async (req, res) => {
   const user = new User(req.body)
   try {
@@ -12,6 +12,20 @@ router.post('/users', UserController.checkIfUserExist, async (req, res) => {
     const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
     res.status(201).json({
       status: 201,
+      token: token
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+// for user login
+router.post('/users/login', UserController.verifyUser, async (req, res) => {
+  const user = req.user
+  try {
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
+    res.status(200).json({
+      status: 200,
       token: token
     })
   } catch (error) {
