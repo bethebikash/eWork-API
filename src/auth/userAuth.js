7bbb012388a9452ea3e7775ec5b8da4a089a7333
bethebikash <bhattaraibikash55@gmail.com>
2020-09-23 00:11:24 +0545

    setting for production environment again

--- src/auth/userAuth.js
+++ src/auth/userAuth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
+
 const userAuth = async (req, res, next) => {
   try {
     const authHeader = req.header('Authorization')
